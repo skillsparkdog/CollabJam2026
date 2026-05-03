2a398fadcf207df6171a3c9a28c782b9790b4e39
hanneballe <hannes.a.jansson@gmail.com>
2026-05-03 20:12:05 +0200

    my changes

---
+++ Assets/fwa/player movement.cs
@@ -0,0 +1,67 @@
+using System.Numerics;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class walk : MonoBehaviour
+{   public float speed;
+    [Range(0f, 1f)]
+public float groundDecay;
+public Rigidbody2D body;
+ public BoxCollider2D groundCheck;
+ public LayerMask groundMask;
+public bool grounded;
+
+float xInput;
+float yInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+void Update()
+    {
+    GetInput();
+    MoveWithInput();
+    }
+
+void FixedUpdate(){
+    CheckGround();
+    ApplyFriction();
+}
+
+void GetInput(){
+    xInput = Input.GetAxis("Horizontal");
+    yInput = Input.GetAxis("Vertical");
+
+}
+    
+void MoveWithInput(){
+    
+if (Mathf.Abs(xInput) > 0) {
+        
+        body.linearVelocity = new UnityEngine.Vector2(xInput * speed, body.linearVelocityY);   
+        }
+
+
+if (Mathf.Abs(yInput) > 0 && grounded) {
+        
+       body.linearVelocity = new UnityEngine.Vector2(body.linearVelocityX, yInput * speed);     
+        }
+}
+    
+
+void CheckGround(){
+        grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
+}
+
+void ApplyFriction(){
+    
+        if(grounded && xInput == 0 && yInput == 0) {
+      body.linearVelocity *= groundDecay;
+    }
+}
+
+}
